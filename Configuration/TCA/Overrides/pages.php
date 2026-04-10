31922aa04c8ec4f92d4194c0069ec06256ff83e7
Thomas Johannknecht <mindbuilding@mailbox.org>
2026-04-10 10:53:14 +0200

    Initialer Upload meiner angepassten Genf Extension

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,22 @@
+<?php
+    defined('TYPO3') or die('Access denied.');
+
+call_user_func(function() {
+    $extensionKey = 'genf';
+
+    // Registrierung der einzelnen Dateien zur manuellen Auswahl
+    $configs = [
+        'Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts/default.tsconfig' => 'Backend Layout: Default',
+        'Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts/kasten.tsconfig' => 'Backend Layout: Kasten',
+        'Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts/FooterBackendLayout.tsconfig' => 'Backend Layout: Footer',
+        'Configuration/TsConfig/Page/old_page.tsconfig' => 'Genf: Element-Anpassungen (TCEFORM)'
+];
+
+    foreach ($configs as $path => $title) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+            $extensionKey,
+            $path,
+            $title
+        );
+    }
+});
---
+++ ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+	defined('TYPO3') or die('Access denied.');
+
+/***************
+ * Add default RTE configuration
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['genf'] = 'EXT:genf/Configuration/RTE/Genf.yaml';
